0a5e93f10ad9a0942fce459ab99cfdc3533ba3cc
Narasimha <narasimhaanguluri001@gmail.com>
2025-01-15 10:32:24 +0530

    change in package configure

--- JdbcConnections/SQL/mySQL.java
+++ JdbcConnections/SQL/mySQL.java
@@ -1,8 +1,6 @@
-package com.JdbcConnections;
+package com.JdbcConnections.SQL;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class mySQL {
     private static String DB_URL = "jdbc:mysql://localhost:3306/EMPLOYEE";
