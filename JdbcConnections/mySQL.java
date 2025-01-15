03d3022b6eda3176961fce6d07285c8544460306
Narasimha <narasimhaanguluri001@gmail.com>
2025-01-15 10:29:06 +0530

    feat: Add jdbc connection and perform Curd operations

---
+++ JdbcConnections/mySQL.java
@@ -0,0 +1,20 @@
+package com.JdbcConnections;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class mySQL {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/EMPLOYEE";
+    private static String DB_user = "******";
+    private static String DB_pass = "********";
+    private Connection connection;
+
+    public mySQL() {
+        try {connection = DriverManager.getConnection(DB_URL, DB_user, DB_pass);
+            System.out.println("Connected Success");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
